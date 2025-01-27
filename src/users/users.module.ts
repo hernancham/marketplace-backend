5bb3d560f30e16f1324a680ca90b20ad0f24e4fc
Hernan_Cham <hachambillac@unjbg.edu.pe>
2025-01-27 13:21:27 -0500

    Eliminar referencia circular a AuthModule en el módulo de usuarios

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [TypeOrmModule.forFeature([User])],
   exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService],
