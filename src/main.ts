85e56fc2dd132f7decf8e88325ff75bcd8d495b3
Hernan_Cham <hachambillac@unjbg.edu.pe>
2025-01-29 23:06:54 -0500

    Agregar configuración de prefijo global, validaciones y CORS en el servidor

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Habilitar prefijo global para las rutas de la API
   app.setGlobalPrefix("api/v1");
 
+  // Habilitar validaciones globales
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +17,15 @@ async function bootstrap() {
     })
   );
   
+  // Habilitar CORS de forma abierta
+  app.enableCors({
+    origin: '*', // Permitir cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Habilita autenticación con tokens si es necesario
+  });
+
+  // Iniciar servidor en el puerto especificado
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
